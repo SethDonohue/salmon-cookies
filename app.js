a879c63c3c95ae236b4c4285ee7a812062f63fef
SethDonohue <donohue.seth@gmail.com>
2017-09-25 17:59:36 -0700

    added Totals Render and Calcualtion functions to branch.

--- app.js
+++ app.js
@@ -10,48 +10,71 @@ function random(min, max){
   // return x;
 }
 
-
 var pike = {
   minCust: 23,
   maxCust: 65,
   averageCookieSale: 6.3,
-  customerEachHour: [],
-
+  // customerEachHour: [],
+  pikeData: [],
+  //Calc Total Cookies
+  totalCookies: function() {
+    var sum = 0;
+    for (var i = 0; i < this.pikeData.length; i++){
+      sum += this.pikeData[i];
+    }
+    // console.log(sum);
+    return sum;
+  },
   //calculated data with methods
   // customersHour
-  customersHour: function() {
-    for (var i = 0; i < hours.length; i++) {
-      //add a random number to customerHour Array
-      console.log('Customer Each Hour: ' + random(pike.minCust, pike.maxCust));
-      this.customerEachHour.push(Math.floor(random(pike.minCust, pike.maxCust)));
-    }
-    console.log(this.customerEachHour);
-  },
+  // customersHour: function() {
+  //   for (var i = 0; i < hours.length; i++) {
+  //     //add a random number to customerHour Array
+  //     console.log('Customer Each Hour: ' + random(pike.minCust, pike.maxCust));
+  // this.customerEachHour.push(Math.floor(random(pike.minCust, pike.maxCust)));
+  // }
+  // console.log(this.customerEachHour);
+  // },
 
-
-
-  // console.log(customerEachHour);
-  // Array of Customers / Hour
-  //cookies per Hour
-  // cookiesHour: customerHour * averageCookieSale,
-
-  // Render function to creat LISTS
-  render: function(){
+  // Render function to create LISTS in HTML
+  renderHourly: function(){
     // console.log(pike.minCust);
     for (var i = 0; i < hours.length; i++) {
       // create a new HTML element
       var liEl = document.createElement('li');
+      //store the cookies per hour for  use in multiple places
+      var hourlyCookies = Math.floor(random(this.minCust, this.maxCust) * this.averageCookieSale);
+
+      // push cookies per hour data into array;
+      this.pikeData.push(hourlyCookies);
+
       // give that element content
-      liEl.textContent = hours[i] + ': ' + Math.floor(random(pike.minCust, pike.maxCust) * this.averageCookieSale) + ' cookies';
+      liEl.textContent = hours[i] + ': ' + hourlyCookies + ' cookies';
+
       // append that element to the right spot in the DOM
       // parentElement.appendChild(childElement);
       var pikeUl = document.getElementById('pike');
       pikeUl.appendChild(liEl);
-      // console.log(pike.minCust);
-      // console.log(pike.maxCust);
     }
+  },
+
+  renderTotal: function() {
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total: ' + this.totalCookies() + ' cookies';
+    var pikeUl = document.getElementById('pikeTotal');
+    pikeUl.appendChild(liEl);
+    // console.log(this.totalCookies());
   }
 };
-//output / render method
-pike.customersHour();
-pike.render();
+
+
+
+
+
+//Output / render method for all objects/lists
+pike.renderHourly();
+pike.renderTotal();
+
+console.log('PikeData Array: ' + pike.pikeData);
+
+console.log('Pike Total Cookies; ' + pike.totalCookies());
